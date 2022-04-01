c8f29d77ef9d25b539a0b9b6dd5ec4bc19d3877b
PaulaMz <64453516+pmezzomo@users.noreply.github.com>
2022-03-31 22:54:03 -0300

    Create operadoresRelacionais.js

---
+++ operadoresRelacionais.js
@@ -0,0 +1,18 @@
+let a = 1
+let b = 2
+
+//Expressoes que o resultado seja true
+console.log(a < b)
+console.log(a <= b)
+console.log(a != b)
+console.log(a < b)
+console.log(a !== b)
+console.log(a + a === b)
+
+//Expressoes que o resultado seja false
+console.log("Aqui tem os Falsos")
+console.log(a > b)
+console.log(a === b)
+console.log(a == b)
+console.log(a >= b)
+console.log(a + a !== b)
