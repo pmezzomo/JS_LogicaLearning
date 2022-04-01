0d02aaaa9151cccc45f88918597e2ba50ff97b3c
PaulaMz <64453516+pmezzomo@users.noreply.github.com>
2022-03-31 23:00:25 -0300

    Create forArray.js

---
+++ forArray.js
@@ -0,0 +1,12 @@
+const numeros = [1,2,3,4,5,6,9,10  ]
+
+// for(let i = 0; i < numeros.length; i++)
+//Percorre de 2 em 2:
+
+for(let i = 0; i < numeros.length; i += 2) {
+    console.log(i, numeros[i])
+}
+
+// for(let i = 0; i<9; i++){
+//     console.log(i, numeros[i])
+// }
