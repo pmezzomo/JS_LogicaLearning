c2593ef7eb8cbe42683c7f4ad5fdb501656d8c72
PaulaMz <64453516+pmezzomo@users.noreply.github.com>
2022-03-31 22:50:46 -0300

    Create changeValue.js

---
+++ changeValue.js
@@ -0,0 +1,14 @@
+let a = 7
+let b = 94
+
+//Variavel temporaria para armazenar o valor inicial de a
+// let temp = a
+// a = b
+// b = temp
+
+[a, b] = [b, a]
+
+// console.log(temp)
+console.log(a)
+console.log(b)
+
