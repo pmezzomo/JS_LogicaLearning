c4479615f9ca7c61a8d708435243bf6283162d35
PaulaMz <64453516+pmezzomo@users.noreply.github.com>
2022-03-31 22:51:04 -0300

    Create ternario.js

---
+++ ternario.js
@@ -0,0 +1,17 @@
+// const hora = 19
+
+// // const saudacao = hora >= 12 ? "Boa tarde" : "Bom dia"
+// // console.log(saudacao)
+
+// const saudacao2 = hora < 11 ? "BOM DIA!" : (hora <= 18 ? "Boa tarde" : "Boa noite")
+// console.log(saudacao2)
+
+const a = 8
+const b = 4
+const operacao = '/'
+
+const resultado = operacao === '+' ? a + b : 
+                    operacao === '-' ? a + b :
+                        operacao === '*' ? a * b : a / b
+console.log(resultado)
+
