d57ed73ed8c8618a88701b083918f15829a65f01
PaulaMz <64453516+pmezzomo@users.noreply.github.com>
2022-03-31 22:50:15 -0300

    Create while.js

---
+++ while.js
@@ -0,0 +1,13 @@
+// while(true) {
+//     console.log("Laço infinito!!")
+// }
+
+// console.log("Fim")
+let quantidade = 0 
+while(Math.random() < 0.97) {
+    quantidade++
+}
+
+console.log("Qdtde " + quantidade)
+
+console.log(Date.now())
